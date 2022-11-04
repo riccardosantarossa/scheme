73b7554a7d5674637a4acae3c38ea07fc69f01f7
Riccardo Santarossa <brudo1968@libero.it>
2022-11-04 08:32:50 +0100

    Fatta la prima parte

--- convertitore.rkt
+++ convertitore.rkt
@@ -6,18 +6,20 @@
    ("") )
   )
 
-
-(define converti ;val : numero intero o decimale
-  (lambda (n)
-    (if (> (string-length n )1 )
-        (+
-         (string->number (expt 2 (string-length n)))
-         (converti (substring n 1))
-         )
-        (if (string=? n "1") ;sequenza di un solo numero
-            (string-append "1")
-            (string-append "0")
-        )
-     )
+(define intera ;val : numero intero
+  (lambda (n)  ;numero binario in input
+   (if (>= (string-length n) 1)  
+    (+ (* (string->number(substring n 0 1)) (expt 2 (-(string-length n) 1))) (intera(substring n 1 (string-length n) )))
+    0
+   )
   )
 )
+
+(define decimale  ;val : numero decimale
+  (lambda (s)  ;sottostringa presa dal . del numero a dx
+    (if (>= (string-length s) 1) ;calcola usando le potenze negative di 2
+        (+ (* (string->number(substring s (-(string-length s) 1))) (expt 2 (- 0 (string-length s)))) (decimale (substring s 0 (- (string-length s) 1)))) 
+        0
+        )
+    )
+ )
