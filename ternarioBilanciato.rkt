1f80158bc994b3942ffd552f0f4b484bb9414e37
Riccardo Santarossa <brudo1968@libero.it>
2022-11-04 10:34:14 +0100

    Sistema ternario

--- ternarioBilanciato.rkt
+++ ternarioBilanciato.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ternarioBilanciato) (read-case-sensitive #t) (teachpacks ((lib "drawings.ss" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.ss" "installed-teachpacks")) #f)))
+;CONVERTE UN NUMERO INTERO NEL SISTEMA TERNARIO BILANCIATO
+
 (define btr-rep ; val: stringa di +/-/.
   (lambda (n)   ; n: intero
     (let (
