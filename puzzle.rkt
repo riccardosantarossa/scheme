36516fcca33ad64d5de9e14d555970bb4a1fe5d1
Riccardo Santarossa <brudo1968@libero.it>
2022-11-04 10:32:42 +0100

    Disegna due figure

--- puzzle.rkt
+++ puzzle.rkt
@@ -2,10 +2,10 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname puzzle) (read-case-sensitive #t) (teachpacks ((lib "drawings.ss" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.ss" "installed-teachpacks")) #f)))
 
-;DISEGNA UNA CROCE REGOLARE
+; PROCEDURA PER DISEGNARE UNA CROCE REGOLARE
 (glue-tiles (glue-tiles (shift-right (shift-down (half-turn larger-tile) 0.8) 1.6) (shift-down (shift-right (half-turn smaller-tile) 1.6) 4)) (glue-tiles larger-tile (shift-right smaller-tile 1.6)))
 
-;DISEGNA UN QUADRATO INCLINATO
+;PROCEDURA PER DISEGNARE UN QUADRATO INCLINATO
 (glue-tiles (glue-tiles (shift-right  (half-turn larger-tile) 1.4) (shift-right(half-turn smaller-tile) 3)) (glue-tiles (shift-right(shift-down smaller-tile 4) 3) (shift-down  (shift-right  larger-tile 3) 0.8)))
  
 
