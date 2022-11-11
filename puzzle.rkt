07ecd03ea2be2538cde43dd02b32f0764a6b7b80
Riccardo Santarossa <brudo1968@libero.it>
2022-11-11 11:06:37 +0100

    Disegna figure

--- puzzle.rkt
+++ puzzle.rkt
@@ -3,6 +3,8 @@
 #lang racket
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname puzzle) (read-case-sensitive #t) (teachpacks ((lib "drawings.ss" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.ss" "installed-teachpacks")) #f)))
 
+;DISEGNA FIGURE GEOMETRICHE
+
 ; PROCEDURA PER DISEGNARE UNA CROCE REGOLARE
 (glue-tiles (glue-tiles (shift-right (shift-down (half-turn larger-tile) 0.8) 1.6) (shift-down (shift-right (half-turn smaller-tile) 1.6) 4)) (glue-tiles larger-tile (shift-right smaller-tile 1.6)))
 
