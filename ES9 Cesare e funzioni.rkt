534bf608f6dc97f32bcf270b5793d36cbddbeac4
Riccardo Santarossa <brudo1968@libero.it>
2022-12-16 17:37:53 +0100

    Fatta la funzione successivo

--- ES9 Cesare e funzioni.rkt
+++ ES9 Cesare e funzioni.rkt
@@ -2,10 +2,19 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |ES9 Cesare e funzioni|) (read-case-sensitive #t) (teachpacks ((lib "drawings.ss" "installed-teachpacks") (lib "hanoi.ss" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.ss" "installed-teachpacks") (lib "hanoi.ss" "installed-teachpacks")) #f)))
 
-(define succ
+;SECONDA PARTE
+
+;funzione identica
+(define i
   (lambda (x)
-    (+ x 1)
-  )
+    x
+   )
 )
 
-(define add
+;funzione successivo
+(define s2
+  (lambda (i n)
+     (+ (i n) 1)
+   )
+)
+
