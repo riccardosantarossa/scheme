61749ba3487cbcff378a7a9ae4e7f665fa451107
Riccardo Santarossa <brudo1968@libero.it>
2022-12-23 09:22:13 +0100

    Fatti due esercizi

--- esercitazioni 06-12.rkt
+++ esercitazioni 06-12.rkt
@@ -2,35 +2,46 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |esercitazioni 06-12|) (read-case-sensitive #t) (teachpacks ((lib "drawings.ss" "installed-teachpacks") (lib "hanoi.ss" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.ss" "installed-teachpacks") (lib "hanoi.ss" "installed-teachpacks")) #f)))
 
-;ES 4 STRINGHE CICLICHE
+;ES 2 INCREMENTO DI UN NUMERO RAPPRESENTATO SOTTO FORMA DI STRINGHE
+(define offset (char->integer #\0))
 
+(define last-digit
+  (lambda (base) (integer->char (+ (- base 1) offset)) ))
+
+(define next-digit
+  (lambda (dgt) (string (integer->char (+ (char->integer dgt) 1))) ))
+
+(define increment
+  (lambda (num base) ; 2 <= base <= 10
+    (let ((digits (string-length num)))
+      (if (= digits 0)
+          "1"
+          (let ((dgt (string-ref num (- digits 1))))
+            (if (char=? dgt (last-digit base))
+                (string-append (increment (substring num 0 (- digits 1)) base)
+                 "0")
+                (string-append (substring num 0 (- digits 1)) (next-digit dgt) )
+             ))
+))))
+
+
+;ES 4 STRINGHE CICLICHE
 
 ;ES 7 NUMERI COMUNI A DUE LISTE
 
-(define belong? ;val: true/false
-  (lambda (x S) ;x: intero, S: lista di interi
-     (cond
-       ((null? S)
-        false)
-       ((= (car S) x)
-        true)
-       (else
-        (belong? x (cdr S))
-       )
-      )
-   ))
-
-(define L '() )
-
-(define shared
-  (lambda (u v)
-      (if (belong? (car u) v)
-          (begin
-           (cons (car u) L)
-           (shared (cdr u) v)
-          (shared (cdr u) v)
-      )
-))
+(define shared  ;val: lista 
+  (lambda (u v) ;u, v: liste di interi ordinati
+    (cond ((or (null? v) (null? u))
+           null)
+          ((= (car u) (car v))
+            (cons (car u) (shared (cdr u) (cdr v))))  
+          ((< (car u) (car v))
+           (shared (cdr u) v))
+          (else
+           (shared u (cdr v)))
+    )
+  )
+)
 
 
 
