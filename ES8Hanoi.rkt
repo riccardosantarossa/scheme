5c5f2edd825ace04d13aa2aca6190cc0efd41c11
riccardosantarossa <rikysanta03@gmail.com>
2023-01-13 18:11:44 +0100

    Almeno c'è

--- ES8Hanoi.rkt
+++ ES8Hanoi.rkt
@@ -2,38 +2,48 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ES8Hanoi) (read-case-sensitive #t) (teachpacks ((lib "drawings.ss" "installed-teachpacks") (lib "hanoi.ss" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.ss" "installed-teachpacks") (lib "hanoi.ss" "installed-teachpacks")) #f)))
 
-;DEFINISCE LA LISTA DI MOSSE NECESSARIA ALLA RISOLUZIONE DEL PROBLEMA
-(define hanoi-moves ; val: lista di coppie
- (lambda (n) ; n > 0 intero
- (hanoi-rec n 1 2 3)
- ))
+(define hanoi-cont ; val: lista di coppie
+  (lambda (n s d t k h v a) ; n intero, s, d, t: posizioni
+    (if (= n 1)
+        (if (> v k)
+            0
+            (cond ((equal? s h) -1)
+                  ((equal? d h) +1)
+                  (else 0))
+            )
+        (let (
+              (m1 (hanoi-cont (- n 1) s t d k h (- v a) (quotient a 2) ))
+              (m2 (hanoi-cont (- n 1) t d s k h (+ v a) (quotient a 2) ))
+              )
+          (+ m1 (+
 
-;PROCEDURA CHE OPERA SULLE LISTE PER CREARE LE COPPIE DISCO/ASTICELLA
-(define hanoi-rec ; val: lista di coppie
- (lambda (n s d t) ; n intero, s, d, t: posizioni
-   (if (= n 1)
-       (list (list s d))
-       (let ((m1 (hanoi-rec (- n 1) s t d))
-             (m2 (hanoi-rec (- n 1) t d s))
-             )
-         (append m1 (cons (list s d) m2))
-       )
-    )
- ))
-
-;PROCEDURA CHE DETERMINA LA CONFIGURAZIONE DEI DISCHI ALLA K-ESIMA MOSSA 
-
-(define hanoi-disks  ;val: lista di coppie
-  (lambda (n k)      ;n, k: interi (n=num dischi, k=num mosse)
-    (config-mosse n k 1 2 3)
-  )
-)
-
-(define config-mosse ;val: lista di coppie
-  (lambda (n k s d t) ;n, k: interi (s, d, t posizione delle asticelle) 
-    (if (= k 0)
-        (append (list (list s n) (list d 0) (list t 0)))
-        ""
+            (if (> v k)
+            0
+            (cond ((equal? s h) -1)
+                  ((equal? d h) +1)
+                  (else 0))
+            )
+            
+            m2))
+          
+          )
+        )
     )
   )
-)
+
+
+(define (hanoi-list n k h)
+  (let ((m (expt 2 (- n 1))))
+    (if (equal? h 1)
+      (+ (hanoi-cont n 1 2 3 k h m (quotient m 2)) n)
+      (hanoi-cont n 1 2 3 k h m (quotient m 2)))
+    ))
+
+
+(define (hanoi-disks n k)
+  (list
+   (list 1 (hanoi-list n k 1))
+   (list 2 (hanoi-list n k 2))
+   (list 3 (hanoi-list n k 3))
+   )
+  )
